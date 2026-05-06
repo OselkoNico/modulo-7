d409664ddf86c9c5d96d1db6d3975f6e037c4682
Osel Nicolás <oselko123@gmail.com>
2026-05-06 16:15:20 +0200

    commit de lo que faltaba

--- practica-1/app.js
+++ practica-1/app.js
@@ -1,9 +1,32 @@
+import mysql from 'mysql2';
 import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/users', (req, res) => {
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "admin",
+    database: "shop"
+})
 
+connection.connect(() => {
+    console.log("Database connected");
+})
+
+app.get('/users', (req, res) => {
+    connection.query("SELECT * FROM users", function (err, result) {
+        if(err) {
+            console.log(err)
+            return res.status(500).json({
+                message: 'Database server error, try again later'
+            })
+        }
+        res.status(200).json({
+            message: 'Ok',
+            result
+        })
+    })
 })
 
 app.listen(port, () => {
